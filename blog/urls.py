509c3814c573f730165d7317534b69c375ef5505
hailong.dao <hailong.dao@ved.com.vn>
2018-02-28 14:19:21 +0700

    add code

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .models import Post
+from django.views.generic import ListView, DetailView
 
 urlpatterns = [
-    path('', views.list, name='blog'),
-    path('<int:id>', views.post, name='post')
+    path('', ListView.as_view(
+        queryset = Post.objects.all().order_by("-date"),
+        template_name = 'blog/blog.html',
+        context_object_name = 'Posts',
+        paginate_by = 4
+    ), name='blog'),
+    path('<int:pk>', views.PostDetailView.as_view(), name='post')
 ]
